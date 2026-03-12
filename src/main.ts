e4520c6486c498660be52466a9f81835ccf10e8c
viinnoo <vinokazu_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2026-03-12 10:41:13 +0700

    fix railway port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
-  async function bootstrap() {
+async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.useGlobalPipes(new ValidationPipe());
@@ -18,6 +18,11 @@ import { ValidationPipe } from '@nestjs/common';
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port, '0.0.0.0');
+
+    console.log(`Server running on port ${port}`);
 }
 bootstrap();
+
